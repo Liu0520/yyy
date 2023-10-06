167a8068a40c6f3c02af04567185803b2b6387dc
Liu0520 <2403028321@qq.com>
2023-10-06 16:30:03 +0800

    配置好主要页面结构

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import React, { memo } from 'react'
+import Home from './views/home'
+import Detail from './views/detail'
+import Entire from './views/entire'
+
 
 const App = memo(() => {
   return (
-    <div>App</div>
+    <div>App
+      <Home></Home>
+      <Detail/>
+      <Entire/>
+    </div>
   )
 })
 
