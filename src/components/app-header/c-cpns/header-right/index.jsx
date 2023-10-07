2cf80ab40933ededb77da969cbbfb613c08df0b3
Liu0520 <2403028321@qq.com>
2023-10-07 18:05:48 +0800

    右侧布局完成

--- src/components/app-header/c-cpns/header-right/index.jsx
+++ src/components/app-header/c-cpns/header-right/index.jsx
@@ -1,9 +1,25 @@
 import React, { memo } from 'react'
 import { RightWrapper } from './style'
+import IconGlobal from '@/assets/svg/icon_global'
+import IconMenu from '@/assets/svg/icon_menu'
+import IconAvatar from '@/assets/svg/icon_avatar'
 
 const HeaderRight = memo(() => {
   return (
-    <RightWrapper>rightwrapper</RightWrapper>
+    <RightWrapper>
+      <div className="btns">
+        <div className="btn">登录</div>
+        <div className="btn">注册</div>
+        <div className="btn">
+          <IconGlobal/>
+        </div>
+      </div>
+
+      <div className="profile" >
+        <IconMenu/>
+        <IconAvatar/>
+      </div>
+    </RightWrapper>
   )
 })
 
--- src/components/app-header/c-cpns/header-right/style.js
+++ src/components/app-header/c-cpns/header-right/style.js
@@ -4,4 +4,44 @@ export const RightWrapper = styled.div`
   flex: 1;
   display: flex;
   justify-content: flex-end;
+  align-items: center;
+
+  color: ${props => props.theme.text.primaryColor};
+  font-weight: 600;
+
+  .btns {
+    display: flex;
+
+    .btn {
+      height: 18px;
+      line-height: 18px;
+      padding: 12px 15px;
+      border-radius: 22px;
+      cursor: pointer;
+
+      &:hover {
+        background-color: #f5f5f5;
+      }
+      
+    }
+  }
+
+  .profile {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    width: 77px;
+    height: 42px;
+    margin-right: 24px; 
+
+    /* box-sizing: border-box; */
+    border: 1px solid #ccc;
+    border-radius: 25px;
+    background-color: #fff;
+    color: ${props => props.theme.text.primaryColor};
+    cursor: pointer;
+
+
+
+  }
 `
